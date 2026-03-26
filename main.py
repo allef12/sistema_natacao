fa74bf08f414ecac1fd6bc77ee089602c73628b3
allef12 <abueno297@gmail.com>
2026-03-26 16:07:55 -0300

    Botão
    Objeto botao1 para guardar método Button, tem a função de abrir o cadastro

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ janela.geometry('500x400')
 
 titulo = tk.Label(janela , text='Sistema Natação Mariana', font=('Arial',20))
 titulo.pack(pady=20)
+
+botao1 = tk.Button(janela, text='Cadastrar Aluno', width=20, command=abrir_cadastro)
+botao1.pack()
