7218883b5b7029889a5f8907407d5af9a41c5cbb
allef12 <abueno297@gmail.com>
2026-03-26 15:30:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ import tkinter as tk
 janela = tk.Tk()
 janela.title('Sistema de Natação')
 janela.geometry('500x400')
+
 titulo = tk.Label(janela , text='Sistema Natação Mariana', font=('Arial',20))
 titulo.pack(pady=20)
