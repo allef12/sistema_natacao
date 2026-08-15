d109c6894455553b9b946d174b60f95078a8cd6e
ALLEF BUENO <abueno297@gmail.com>
2026-08-15 01:44:55 -0300

    bibliotecas na tela de excluir
    importando bibliotecas

---
+++ telas/excluir_aluno.py
@@ -0,0 +1,13 @@
+# importa interface gráfica
+import tkinter as tk
+
+# importa combobox
+from tkinter import ttk
+
+# importa caixa de mensagem
+from tkinter import messagebox
+
+# importa conexão com o banco
+from database import conectar
+
+
