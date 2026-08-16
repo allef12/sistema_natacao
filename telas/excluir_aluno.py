0227025f8d5b03e8465766275bcc6306a4bf486c
ALLEF BUENO <abueno297@gmail.com>
2026-08-16 03:09:46 -0300

    Abrindo interface
    Usando a função pra abrir a janela, métodos executados vão gerar interface gráfica

--- telas/excluir_aluno.py
+++ telas/excluir_aluno.py
@@ -2,3 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from database import conectar
+    
+def abrir_excluir_aluno():
+    #abrir tela
+    tela = tk.Tk()
+    tela.title("Excluir aluno")
+    tela.geometry("400x300")
