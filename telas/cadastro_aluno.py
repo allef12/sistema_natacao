e05ec3728eaccd7f939368e9dd9068768645d9be
ALLEF BUENO <abueno297@gmail.com>
2026-08-16 14:34:19 -0300

    Update cadastro_aluno.py

--- telas/cadastro_aluno.py
+++ telas/cadastro_aluno.py
@@ -75,45 +75,8 @@ def abrir_cadastro():
     text='Salvar',
     command=clicar_salvar
 
-    tk.Label(tela, text='Nome do Aluno').pack(pady=10)
 
-    entrada_nome = tk.Entry(tela)
-    entrada_nome.pack()
 
-    tk.Label(tela, text='Telefone').pack(pady=10)
-
-    entrada_tel = tk.Entry(tela)
-    entrada_tel.pack()
-
-    tk.Label(tela, text='Data de nascimento').pack(pady=10)
-
-    entrada_nasc = tk.Entry(tela)
-    entrada_nasc.pack()
-
-    def clicar_salvar():
-
-        nome = entrada_nome.get()
-        telefone = entrada_tel.get()
-        data_nascimento = entrada_nasc.get()
-
-        if nome == "" or telefone == "" or data_nascimento == "":
-            messagebox.showwarning('Aviso','Preencha todos os campos')
-
-            return
-
-        salvar_aluno(nome, telefone, data_nascimento)
-
-        messagebox.showinfo('Sucesso', 'Aluno cadastrado com sucesso!')
-
-        entrada_nome.delete(0, tk.END)
-        entrada_tel.delete(0, tk.END)
-        entrada_nasc.delete(0, tk.END)
-
-        entrada_nome.focus_set()
-
-    
-
-    tk.Button(tela, text='Salvar', command= clicar_salvar).pack(pady=10)
 
 
 
