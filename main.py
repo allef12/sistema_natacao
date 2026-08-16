33bcca49a2d44e511bd89b81b41734a6eb9ef71e
ALLEF BUENO <abueno297@gmail.com>
2026-08-16 14:25:50 -0300

    Alteração
    fiz uma alteração no tamanho da janela

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from telas.editar_aluno import abrir_editar_alunos
 
 janela = tk.Tk()
 janela.title('Sistema de Natação')
-janela.geometry('500x400')
+janela.geometry('800x600')
 
 titulo = tk.Label(janela , text='Sistema Natação Mariana', font=('Arial',20))
 titulo.pack(pady=20)
