07824dcdbd35ed2a4fe7d239962790beeee26f26
allef12 <abueno297@gmail.com>
2026-09-01 08:32:51 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,11 @@
 
 from fastapi import FastAPI
+from database import conectar
 
 app = FastAPI() # quando alguém acessa, o app recebe a requisição
 
 @app.get("/") # registra uma rota para requisições GET (buscar informações).
 def inicio(): #associa a função a rota
-    return{"mensagem":"API funcionando carai"}
+    return{"mensagem":"API funcionando"}
 
 
