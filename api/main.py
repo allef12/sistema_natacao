78017c4fafe9a6e1ad02b48a6001d0425ea5b9d7
allef12 <abueno297@gmail.com>
2026-09-03 14:13:33 -0300

    Rota alunos
    Aqui nesse caso é a mesma coisa, é registrado uma requisição do tipo get com uma rota pra alunos, é entregue para FastAPI  e executado a função que conecta ao banco e busca os alunos

--- api/main.py
+++ api/main.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 def inicio():
     return{"mensagem":"API funcionando"}
+
+
+@app.get("/alunos")
+def alunos():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""SELECT id,nome,telefone,data_nascimento FROM alunos""")
+
+    alunos = cursor.fetchall()
+
+    conn.close()
