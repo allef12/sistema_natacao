3fd9851e6d0cc8d50c28afae7aef0ed5ecac38c2
allef12 <abueno297@gmail.com>
2026-09-03 10:02:33 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,23 +1,3 @@
-
 from fastapi import FastAPI
 from database import conectar
-
-app = FastAPI() # quando alguém acessa, o app recebe a requisição
-
-@app.get("/") # registra uma rota para requisições GET (buscar informações).
-def inicio(): #associa a função a rota
-    return{"mensagem":"API funcionando"}
-
-@app.get("/alunos")
-def listar_alunos():
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute("""SELECT id,nome,telefone,data_nascimento
-                      FROM alunos""")
-
-    alunos = cursor.fetchall()
-
-    conn.close
-
 
