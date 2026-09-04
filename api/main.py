b92c68bb11e05f2109f13d39dd0de089a5fd07a3
allef12 <abueno297@gmail.com>
2026-09-04 16:18:14 -0300

    Definindo rotas
    Aqui estou criando 2 rotas para busca de alunos e pagamentos, usando a pasta routers e importando os objetos e importando as rotas dentro desses objetos

--- api/main.py
+++ api/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-
+from routers import alunos
+from routers import pagamentos
 
 app = FastAPI()
 
 @app.get("/")
 def inicio():
     return{"mensagem":"API funcionando"}
+
+
+app.include_router(alunos.router)
+
+app.include_router(pagamentos.router)
 
