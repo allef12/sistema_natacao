141b5ff13d05c34fe28fab5367345a83763a942e
allef12 <abueno297@gmail.com>
2026-09-04 10:28:06 -0300

    Update main.py
    Mudança relacionada a inclusão do módulo router que vai ser vir para gerenciar melhor as rotas a medida que o projeto cresce

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from database import conectar
+
 
 app = FastAPI()
 
@@ -7,27 +7,3 @@ app = FastAPI()
 def inicio():
     return{"mensagem":"API funcionando"}
 
-
-@app.get("/alunos")
-def alunos():
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute("""SELECT id,nome,telefone,data_nascimento FROM alunos""")
-
-    alunos = cursor.fetchall()
-
-    conn.close()
-
-    resultado = []
-
-    for aluno in alunos:
-        dados = {
-            "id":aluno.id,
-            "nome":aluno.nome,
-            "telefone":aluno.telefone,
-            "data_nascimento":aluno.data_nascimento
-        }
-        resultado.append(dados)
-
-    return resultado
