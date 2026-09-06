24801d04b0ae9fe85cf2ade703e7c28ddf14a2ba
ALLEF BUENO <abueno297@gmail.com>
2026-09-05 21:36:47 -0300

    Decorador e função
    O decorador registra a operação, caminho e função a executar, a função que vai ser executada é para conectar ao banco e fazer uma consulta, por isso é usado o método get usando o caminho /alunos

--- api/routers/alunos.py
+++ api/routers/alunos.py
@@ -3,3 +3,15 @@ from database import conectar
 
 router = APIRouter()
 
+@router.get("/alunos")
+def listar_alunos():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""SELECT id,nome,telefone,data_nascimento
+                      FROM alunos""")
+
+    alunos = cursor.fetchall()
+
+    conn.close()
+
