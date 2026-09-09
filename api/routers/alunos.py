d6b86da40e52ebd2af309fdf3e7f415e35cd1e02
ALLEF BUENO <abueno297@gmail.com>
2026-09-08 22:34:37 -0300

    Rota específica
    Rota de aluno específico

--- api/routers/alunos.py
+++ api/routers/alunos.py
@@ -30,3 +30,25 @@ def listar_alunos():
 
 @router.get("/alunos/{id}")
 def buscar_alunos(id: int):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""SELECT id,nome,telefone,data_nascimento
+                       FROM alunos
+                       WHERE id = ?""",(id,))
+    
+    aluno = cursor.fetchone()
+    conn.close()
+
+    if aluno is None:
+        return {"mensagem":"Não foi encontrado aluno"}
+
+    dados = {
+        "id":aluno.id,
+        "nome": aluno.nome,
+        "telefone": aluno.telefone,
+        "data_nascimento": aluno.data_nascimento
+        
+    }
+    return dados
+    
