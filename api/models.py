e0ad2a4a07a926bdbd0a8436021753af89bf7e79
ALLEF BUENO <abueno297@gmail.com>
2026-09-23 23:37:05 -0300

    Conjunto pré-definido de valores permitidos

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from datetime import date
 from typing import Optional
 from pydantic import BaseModel, Field
+from enum import Enum
 
 class Aluno(BaseModel):
     nome: str
@@ -12,4 +13,9 @@ class Pagamento(BaseModel):
     aluno_id: int
     mes: str = Field(min_length=3, max_length=20)
     valor:float = Field(gt=0)
+    
+
+class StatusPagamento(str, Enum):
+    pago = "Pago"
+    pendente = "Pendente"
     
